632af0c9f07b3cf425231660282ccb7755eb1711
Your Namegit config --global user.name Veronika <veronika.tarasova.98@gmail.com>
2022-09-23 17:43:22 +0300

    edited method removeLocalMaxima

--- src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
+++ src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
@@ -11,9 +11,17 @@ public class LocalMaximaRemove {
     }
 
     public static int[] removeLocalMaxima(int[] array){
-
+         int [] result = new int[array.length];
         //put your code here
+        int j = 0;
 
-        throw new UnsupportedOperationException();
+        for(int i = 0; i < array.length; i++) {
+            if ((i != (array.length - 1) && array[i] <= array[i + 1]) || i > 0 && array[i] <= array[i - 1]) {
+             result[j] = array[i];
+             j++;
+            }
+        }
+
+        return Arrays.copyOf(result, j);
     }
 }
